4bb57d21c70ad0313e37120bf99bfb120e58c088
RonJurincie <ronjurincie@gmail.com>
2024-07-07 16:36:19 -0400

    Initial Commit

---
+++ CVS-Challenge/CVS_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CVS_ChallengeApp.swift
+//  CVS-Challenge
+//
+//  Created by Ron Jurincie on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CVS_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
